5c517274fd8974b9b87d879104c908d4d2ef7f21
rdavid20? <73649411+rdavid20@users.noreply.github.com>
2021-05-10 12:52:37 +0200

    Add files via upload

--- MainPC.py
+++ MainPC.py
@@ -7,10 +7,24 @@ import sqlite3 as lite
 import sys
 
 def VideoFeed():
+    cv2.namedWindow('window')
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (600, 15)
+    fontScale = 0.5
+    color = (255, 255, 255)
+    thickness = 1
+
     while True:
         img = tello.get_frame_read().frame
-        cv2.imshow("Live Stream", img)
-        cv2.waitKey(1)
+        img = cv2.resize(img, (720,480))
+        string = 'Battery: {}%'.format(tello.get_battery())
+        img = cv2.putText(img, string, org, font,
+        				fontScale, color, thickness, cv2.LINE_AA)
+        cv2.imshow("window", img)
+        key = cv2.waitKey(3)
+        if key == 27:
+            break
 
         if(keyboard.is_pressed('r') == True):
             tello.streamoff()
@@ -20,6 +34,7 @@ def VideoFeed():
     cv2.destroyAllWindows()
 
 def Controls(speed):
+    landed = True
     while True:
         a = 0
         b = 0
@@ -51,12 +66,25 @@ def Controls(speed):
             d+= -speed
 
         if(keyboard.is_pressed("space") == True):
-            tello.land()
-            sendToDB("land")
-            break
+            if(landed == False):
+                try:
+                    tello.land()
+                    sendToDB("land")
+                    landed = True
+                    time.sleep(4)
+                except:
+                    print("Error: Landing failed")
+            if(landed == True):
+                try:
+                    tello.takeoff()
+                    sendToDB("takeoff")
+                    landed = False
+                except:
+                    print("Error: Takeoff failed")
 
-        tello.send_rc_control(a,b,c,d)
-        sendToDB("rc {} {} {} {}".format(a,b,c,d))
+        if(landed == False):
+            tello.send_rc_control(a,b,c,d)
+            sendToDB("rc {} {} {} {}".format(a,b,c,d))
         time.sleep(0.1)
 
 def loggingToDB():
@@ -82,13 +110,19 @@ def sendToDB(insertThis):
     cur.execute("INSERT INTO Commands(Command) VALUES (?)",(insertThis,))
     con.commit()
 
+def connectToDrone():
+    while True:
+        try:
+            tello.connect()
+            break
+        except:
+            print("Cannot connect to Drone")
+
 tello = tello.Tello()
-tello.connect()
+connectToDrone()
 
 tello.streamon()
 sendToDB("streamon")
-tello.takeoff()
-sendToDB("takeoff")
 
 videoThread = threading.Thread(target=VideoFeed)
 videoThread.start()
