7814e3e8f2ce38b25b663d294d518c5c66732bee
rdavid20? <73649411+rdavid20@users.noreply.github.com>
2021-05-20 10:08:09 +0200

    Update relayState.py

--- Relay Box/relayState.py
+++ Relay Box/relayState.py
@@ -1,13 +1,17 @@
 import threading
 import socket
 import sys
+"""
+test
+"""
 
+pcAddr = ('192.168.137.1', 8890)
 def relayState():
-    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s2.bind(('', 8890))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', 8890))
     while True:
-            data, server = s2.recvfrom(1518)
-            s2.sendto(data, ('192.168.0.144', 8890))
+            data, server = s.recvfrom(1518)
+            s.sendto(data, pcAddr)
 
 relayState()
 
